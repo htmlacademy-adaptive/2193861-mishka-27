9e2f3a774c449a03f8fe7f5bbf8f5d09fefd6f7a
deniskirshenin <deniskirshenin@gmail.com>
2023-01-24 15:01:13 +0300

    Внесены правки после проверки

--- source/js/script.js
+++ source/js/script.js
@@ -2,15 +2,11 @@ let navToggle = document.querySelector('.header-nav__toggle');
 let userMenu = document.querySelector('.header-nav__list-user');
 let siteMenu = document.querySelector('.header-nav__list-site');
 let navLogo = document.querySelector('.header-nav__logo');
-let staticImageMap = document.querySelector('.contacts__map-image');
-let interactiveMap = document.querySelector('.contacts__map');
 
 userMenu.classList.remove('header-nav__list-user--nojs');
 siteMenu.classList.remove('header-nav__list-site--nojs');
 navToggle.classList.remove('header-nav__toggle--nojs');
 navLogo.classList.remove('header-nav__logo--nojs');
-staticImageMap.classList.remove('contacts__map-image--nojs');
-interactiveMap.classList.add('contacts__map--active');
 
 navToggle.addEventListener('click', function () {
   if (userMenu.classList.contains('header-nav__list-user--closed') && siteMenu.classList.contains('header-nav__list-site--closed')) {
@@ -28,4 +24,4 @@ navToggle.addEventListener('click', function () {
     siteMenu.classList.remove('header-nav__list-site--opened');
     siteMenu.classList.add('header-nav__list-site--closed');
   }
-})
+});
